6fe8c3cc84675870539771c0e9a1b141deaab818
boolancpain <boolancpain@gmail.com>
2022-06-10 10:21:10 +0900

    Add: baekjoon 블랙잭 문제풀이

---
+++ src/baekjoon/Test2798.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * 블랙잭
+ * 
+ * @see https://www.acmicpc.net/problem/2798
+ * @author boolancpain
+ */
+public class Test2798 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[] cardArr = new int[n];
+		for(int i = 0;i < n;i++) {
+			cardArr[i] = sc.nextInt();
+		}
+		
+		int sum = 0;
+		int tempSum = 0;
+		for(int i = 0;i < n - 2;i++) {
+			for(int j = i + 1;j < n - 1;j++) {
+				for(int k = j + 1;k < n;k++) {
+					tempSum = cardArr[i] + cardArr[j] + cardArr[k];
+					
+					if(tempSum > sum && tempSum <= m) {
+						sum = tempSum;
+					}
+				}
+			}
+		}
+		
+		System.out.println(sum);
+	}
+}
