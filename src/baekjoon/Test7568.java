a439bbdbe480e6d703f9f025a7bd52c56cd25f29
boolancpain <boolancpain@gmail.com>
2022-06-16 17:19:13 +0900

    Refactor: 주석 추가

--- src/baekjoon/Test7568.java
+++ src/baekjoon/Test7568.java
@@ -17,18 +17,22 @@ public class Test7568 {
 			
 			int[] ranks = new int[n];
 			
+			// 입력
 			for(int[] person : persons) {
 				person[0] = sc.nextInt();
 				person[1] = sc.nextInt();
 			}
 			
+			// 1로 초기화
 			for(int i = 0;i < ranks.length;i++) {
 				ranks[i] = 1;
 			}
 			
+			// 비교
 			for(int i = 0;i < n;i++) {
 				for(int j = 0;j < n;j++) {
 					if(i != j) {
+						// i번째 사람보다 j번째 사람이 덩치 큰 경우에만 i번째 사람의 랭크를 증가시킴
 						if(persons[i][0] < persons[j][0] && persons[i][1] < persons[j][1]) {
 							ranks[i] += 1;
 						}
