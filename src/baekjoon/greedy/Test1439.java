9623ca125541921a7d0cbc032c316ca2e138ff7f
boolancpain <boolancpain@gmail.com>
2022-07-20 10:47:09 +0900

    Add: baekjoon 뒤집기 문제풀이

---
+++ src/baekjoon/greedy/Test1439.java
@@ -0,0 +1,28 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 뒤집기
+ * 
+ * @see https://www.acmicpc.net/problem/1439
+ * @author boolancpain
+ */
+public class Test1439 {
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			String s = br.readLine();
+			
+			StringTokenizer zeroTokenizer = new StringTokenizer(s, "0");
+			StringTokenizer oneTokenizer = new StringTokenizer(s, "1");
+			
+			System.out.println(Math.min(zeroTokenizer.countTokens(), oneTokenizer.countTokens()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
